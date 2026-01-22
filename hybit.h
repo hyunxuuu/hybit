3dd8c99663c182502d1f7c271656cdb13317cca0
hyunxuuu <h3314829906@163.com>
2026-01-22 18:10:48 +0800

    Rename marco FLAG.

--- hybit.h
+++ hybit.h
@@ -15,10 +15,10 @@
 #define HYBIT_SETUP                     (1)
 #define HYBIT_UNSET                     (0)
 
-#define FLAG_SET                        HYBIT_SET
-#define FLAG_CLR                        HYBIT_CLR
-#define FLAG_GET                        HYBIT_GET
-#define FLAG_SETUP                      HYBIT_SETUP
-#define FLAG_UNSET                      HYBIT_UNSET
+#define HYFLAG_SET                      HYBIT_SET
+#define HYFLAG_CLR                      HYBIT_CLR
+#define HYFLAG_GET                      HYBIT_GET
+#define HYFLAG_SETUP                    HYBIT_SETUP
+#define HYFLAG_UNSET                    HYBIT_UNSET
 
 #endif /* HYBIT_H */
